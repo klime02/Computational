c5fde53d1172e90e52f0fd5d01407a5fcbdcb5a1
klime02 <bhargavgarikipati@hotmail.com>
2016-08-21 01:59:37 +0100

    Working Version 1.0

--- EqnSolver.py
+++ EqnSolver.py
@@ -1,6 +1,3 @@
-EqnIn = input('Enter the LHS of the equation: ')
-EqnRHS = int(input("Enter the RHS of the equation :"))
-
 def powerpull(EqnIn):
     powernum = []
     pr = 0
@@ -11,9 +8,10 @@ def powerpull(EqnIn):
         else:
             pr = pr + 1
     return powernum
+
+
 def spacefinder(EqnIn,cr):
     sr = 0
-    print(cr)
     spaceloc = 0
     coenum = []
     while sr < 20:
@@ -25,14 +23,13 @@ def spacefinder(EqnIn,cr):
             break
         else:
             sr +=1
-            print(sr)
     coefficentcount = 0
     coeffcient = 0
-    print(coenum)
     while coefficentcount < len(coenum):
         coeffcient = int(str(coeffcient) + str(coenum[coefficentcount]))
         coefficentcount +=1
     return coeffcient
+
 
 def coepull(EqnIn):
     cr = 1
@@ -46,6 +43,35 @@ def coepull(EqnIn):
     return coeffcients
 
 
+def accuracy(sol,EqnRHS):
+    if abs(EqnRHS-sol) <= 0.001:
+        return EqnRHS
+    else:
+        return sol
+
+
+def calculation(coeffcients,powernum,x):
+    calcount = 0
+    psol = 0
+    while calcount < len(coeffcients):
+        psol = psol +coeffcients[calcount] * pow(x, powernum[calcount])
+        calcount += 1
+    return psol
+
+
+EqnIn = input('Enter the LHS of the equation: ')
+EqnRHS = int(input("Enter the RHS of the equation: "))
 powernum = powerpull(EqnIn)
 coeffcients = coepull(EqnIn)
-print(coeffcients[0],'x^',powernum[0],' + ',coeffcients[1],'x^',powernum[1],' + ',coeffcients[2],'x^',powernum[2],sep='')
+solutions = []
+x = -10000
+while x<10000:
+    sol = calculation(coeffcients,powernum,x)
+    fsol = accuracy(sol,EqnRHS)
+    if fsol == EqnRHS:
+        print(round(x,2),'is a solution')
+        solutions.append(x)
+        x += 0.005
+    else:
+        x += 0.005
+print(solutions)
